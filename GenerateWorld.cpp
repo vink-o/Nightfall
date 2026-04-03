77da89d2da17091f0217dd101974a1e802d3c361
Vinko Brajnov <vbrajnov3@gmail.com>
2026-04-03 20:43:20 +0200

    implement unit instantiation and real-time grid mapping
    Created specialized std::vector containers for all 6 character classes (Lumberjack, Warrior, Healer, Hunter, Builder, Miner).
    Implemented automated spawning logic using for-loops based on predefined population counts.
    Integrated direct mapping of character symbols (L, W, H, h, B, M) onto the Map grid coordinates (25, 50).
    Ensured data synchronization between the memory-stored objects and the visual ASCII output.
    Prepared the architecture for the upcoming movement and AI update loop.

--- GenerateWorld.cpp
+++ GenerateWorld.cpp
@@ -11,24 +11,62 @@ using namespace std;
 
 void GenerateWorld()
 {
+    vector <LUMBERJACK> lumberjack;
+    vector <HEALER> healer;
+    vector <HUNTER> hunter;
+    vector <MINER> miner;
+    vector <BUILDER> builder;
+    vector <WARRIOR> warrior;
+
     Map m;
     Character c;
 
     m.BuildLand();
 
-    LUMBERJACK l(25,50);
-    WARRIOR w (25,50);
-    HEALER h (25,50);
-    HUNTER H (25,50);
-    BUILDER b (25,50);
-    MINER M (25,50);
+    int n_lumberjack=8;
+    int n_healer=3;
+    int n_hunter=6;
+    int n_miner=5;
+    int n_builder=3;
+    int n_warrior=10;
 
-    m.grid[l.GetX()][l.GetY()] = 'L';
-    m.grid[w.GetX()][w.GetY()] = 'W';
-    m.grid[h.GetX()][h.GetY()] = 'h';
-    m.grid[H.GetX()][H.GetY()] = 'H';
-    m.grid[b.GetX()][b.GetY()] = 'B';
-    m.grid[M.GetX()][M.GetY()] = 'M';
+    for(int i=0;i<n_lumberjack;i++)
+    {
+        LUMBERJACK l(25,50);
+        lumberjack.push_back(l);
+        m.grid[l.GetX()][l.GetY()] = 'L';
+    }
+    for(int i=0;i<n_warrior;i++)
+    {
+         WARRIOR w (25,50);
+         warrior.push_back(w);
+         m.grid[w.GetX()][w.GetY()] = 'W';
+    }
+    for(int i=0;i<n_healer;i++)
+    {
+         HEALER h (25,50);
+         healer.push_back(h);
+         m.grid[h.GetX()][h.GetY()] = 'H';
+    }
+    for(int i=0;i<n_hunter;i++)
+    {
+        HUNTER H (25,50);
+        hunter.push_back(H);
+        m.grid[H.GetX()][H.GetY()] = 'h';
+    }
+    for(int i=0;i<n_builder;i++)
+    {
+        BUILDER b (25,50);
+        builder.push_back(b);
+        m.grid[b.GetX()][b.GetY()] = 'B';
+    }
+    for(int i=0;i<n_miner;i++)
+    {
+        MINER M (25,50);
+        miner.push_back(M);
+        m.grid[M.GetX()][M.GetY()] = 'M';
+    }
+
 
     m.Land();
 }
